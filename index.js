166d5eabb9e150d25d46c23afd3a758fae5a3997
Nina Sitaeva <sitaeva.nn@gmail.com>
2023-01-23 15:09:24 +0100

    init: start remote control task

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { mouse } from '@nut-tree/nut-js';
 
 const HTTP_PORT = 8181;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+import { WebSocketServer } from 'ws';
+
+export const wss = new WebSocketServer({ port: HTTP_PORT });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => console.log(data));
+
+  ws.send('hello');
+});
+
+wss.on('close', () => console.log('closed'));
