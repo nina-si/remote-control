c4530b2f91c45256db67197585c3a5cb39ff7c41
Nina Sitaeva <sitaeva.nn@gmail.com>
2023-01-24 21:06:32 +0100

    chore: add scripts and tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import httpServer from './src/http_server/index.js';
-import wsServer from './src/ws_server/index.js';
+import httpServer from './http_server/index';
+import wsServer from './ws_server/index';
 
 const HTTP_PORT = 8181;
 
