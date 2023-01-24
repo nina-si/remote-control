25b444d4ee77b71889fa1440fffbd8f5456f1891
Nina Sitaeva <sitaeva.nn@gmail.com>
2023-01-24 22:07:58 +0100

    feat: add mouse moves

--- src/drawing/mouse.ts
+++ src/drawing/mouse.ts
@@ -1,8 +1,27 @@
 const mousePosition = {
-  x: 15,
+  x: 0,
   y: 0,
 };
 
-export const getMousePosition = () => {
-  return `mouse_position ${mousePosition.x},${mousePosition.y}`;
+export const getMousePosition = (): string => {
+  const x = mousePosition.x;
+  const y = mousePosition.y;
+  return `mouse_position ${x},${y}`;
+};
+
+export const updateMousePosition = (direction: string, step: number): void => {
+  switch (direction) {
+    case 'up':
+      mousePosition.y = mousePosition.y + step;
+      break;
+    case 'down':
+      mousePosition.y = mousePosition.y - step;
+      break;
+    case 'right':
+      mousePosition.x = mousePosition.x + step;
+      break;
+    case 'left':
+      mousePosition.x = mousePosition.x - step;
+      break;
+  }
 };
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,6 +1,6 @@
 import http from 'http';
 import { WebSocketServer } from 'ws';
-import { getMousePosition } from '../drawing/mouse';
+import { getMousePosition, updateMousePosition } from '../drawing/mouse';
 
 const wsServer = http.createServer();
 
@@ -12,11 +12,33 @@ wss.on('connection', function connection(ws, req) {
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
-    const command = data.toString();
-    if (command.startsWith('mouse_position')) {
-      const answer = getMousePosition();
-      ws.send(answer);
-      console.log('sent: ', answer);
+    const command = data.toString().trim();
+    if (command.startsWith('mouse')) {
+      const operation = command.split(' ')[0];
+      let step = 0;
+      switch (operation) {
+        case 'mouse_position':
+          const answer = getMousePosition();
+          ws.send(answer);
+          console.log('sent: ', answer);
+          break;
+        case 'mouse_up':
+          step = Number(command.split(' ')[1]);
+          updateMousePosition('up', step);
+          break;
+        case 'mouse_down':
+          step = Number(command.split(' ')[1]);
+          updateMousePosition('down', step);
+          break;
+        case 'mouse_right':
+          step = Number(command.split(' ')[1]);
+          updateMousePosition('right', step);
+          break;
+        case 'mouse_left':
+          step = Number(command.split(' ')[1]);
+          updateMousePosition('left', step);
+          break;
+      }
     }
   });
 });
